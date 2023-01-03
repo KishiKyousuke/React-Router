2ea107e6a1349932da6a1b2fc19b49e392260c89
KishiKyousuke（Kyo18） <59789739+KishiKyousuke@users.noreply.github.com>
2023-01-03 23:24:12 +0900

    routingの追加

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 import "./styles.css";
 import { Home } from "./Home";
 import { Page1 } from "./Page1";
@@ -5,10 +6,26 @@ import { Page2 } from "./Page2";
 
 export default function App() {
   return (
-    <div className="App">
-      <Home />
-      <Page1 />
-      <Page2 />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/page1">Page1</Link>
+        <br />
+        <Link to="/page2">Page2</Link>
+        <br />
+      </div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/page1">
+          <Page1 />
+        </Route>
+        <Route path="/page2">
+          <Page2 />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
